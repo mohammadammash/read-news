85a568ce15327b00f5f3dc1dcef5933e49f2ff9f
mohammadammash <mohammadammash31@gmail.com>
2022-09-23 15:17:59 +0300

    Intializing '/backend/connection.php' to create connection between our website and local_database 'readnews_db'

---
+++ backend/connection.php
@@ -0,0 +1,13 @@
+<?
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+$host = 'localhost';
+$db_user = 'root';
+$db_pass = null;
+$db_name = 'readnews_db';
+
+// called mysqli constructor - create conx between php file and db
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+?>
