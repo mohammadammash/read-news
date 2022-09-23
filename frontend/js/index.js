4fab15bbc1acbbdf3f3cc882aebebf0becdd0371
mohammadammash <mohammadammash31@gmail.com>
2022-09-23 16:19:25 +0300

    Adding the news fetched (get) elements/data to the main-content of html page by appending them to innerHTML, in '/js/index.js'

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,22 +1,22 @@
+const news_container = document.getElementById("main-content");
 
 // turn each new received from server into html element and push it in the news main container
-const showNews = (news)=>{
-    for(let single_new of news){
-    const newHTML = `<h1 class="display-1 text-center text-uppercase mt-5">all news</h1>
-    <main
-      class="row mx-5 font-monospace justify-content-md-center justify-content-xl-between gap-5 gap-md-2"
-    >
+const showNews = (news) => {
+  let newHTML;
+  for (let single_new of news) {
+    newHTML = `
       <div class="col-12 col-md-5 col-xl-3 border border-2 rounded">
         <h2 class="display-4 text-capitalize text-decoration-underline text-warning">
           ${single_new.title}
         </h2>
         <p class="fs-3 lh-lg">
-          ${single_new.text}
+          ${single_new.description}
         </p>
-        <span class="fs-6 text-muted">~${single_new.date}</span>
-      </div> -->`
-    }
-}
+        <span class="fs-6 text-muted">~${single_new.created_at}</span>
+      </div>`;
+    news_container.innerHTML += newHTML;
+  }
+};
 
 // fetch/get data from all_news_api
 const getAllNews = () => {
@@ -25,8 +25,7 @@ const getAllNews = () => {
   $.getJSON(get_all_news_API, { format: "json" })
     .done((data) => showNews(data))
     .fail((err) => consolee.log(err));
-
 };
 
 // on load get all news from db:
-window.addEventListener('load',getAllNews);
+window.addEventListener("load", getAllNews);
